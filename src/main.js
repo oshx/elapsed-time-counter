5028be483ccf69018dec30e2e3b992dfc02f476a
오승환 <oshxios@gmail.com>
2019-04-20 05:53:57 +0900

    이제는 정상 동작하나?

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
-const GLOBAL_BASE_NAME = '__ELAPSED_TIME_COUNTER_GLOBAL_BASE_TIME__';
-
-module.exports = new (class ElapsedTimeCounter {
-    setStartTime() {
-        const GLOBAL_BASE_TIME = new Date().getTime();
-        if (!window) {
-            return null;
-        }
-        return window[GLOBAL_BASE_NAME] = GLOBAL_BASE_TIME;
+var GLOBAL_BASE_NAME = '__ELAPSED_TIME_COUNTER_GLOBAL_BASE_TIME__';
+var setStartTime = function setStartTime() {
+    const GLOBAL_BASE_TIME = new Date().getTime();
+    if (!window) {
+        return null;
     }
-    getStartTime() {
-        if (!window[GLOBAL_BASE_NAME]) {
-            this.setStartTime();
-        }
-        return window[GLOBAL_BASE_NAME];
+    return window[GLOBAL_BASE_NAME] = GLOBAL_BASE_TIME;
+}
+var getStartTime = function getStartTime() {
+    if (!window[GLOBAL_BASE_NAME]) {
+        setStartTime();
     }
+    return window[GLOBAL_BASE_NAME];
+}
+module.exports = new (function ElapsedTimeCounter() {
+    return {
+        setStartTime,
+        getStartTime
+    };
 })();
